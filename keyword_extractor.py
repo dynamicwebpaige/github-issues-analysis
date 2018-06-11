52d3b465a51fc12eadbcaee9a8c9c0ee24efd4de
Paige Bailey <pabailey@microsoft.com>
2018-06-11 13:05:48 -0700

    Update keyword_extractor.py

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -5,6 +5,7 @@ with open('keras_issues.csv', 'rt', encoding='utf-8', errors='replace') as f:
     reader = csv.reader(f)
     row_text = ""
     for row in reader:
+        row_text += " "
         row_text += row[22]
     
 Rake = RAKE.Rake(RAKE.SmartStopList())
